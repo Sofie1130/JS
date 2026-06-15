e2721a293adb4710a7f669214c603aba32423964
Sofie <wengray62@gmail.com>
2026-06-15 20:36:51 +0800

    1st

---
+++ myjs.js
@@ -0,0 +1,7 @@
+    function changeHeader() {
+        document.getElementById('myh1').style.color='#F08080';
+            }
+  function cleanHeader() {
+        document.getElementById('myh1').style.color='inherit';
+            }
+
